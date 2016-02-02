c84274bce4e3e5cdb95f48c3cac05a9613a7b300
tianbj <tianbj@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2016-02-02 01:39:43 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@32 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Yuanxiang/CheckImagesTool/CheckImagesTool/DBImageCache.cs
@@ -0,0 +1,161 @@
+﻿using SubSonic.Schema;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckImagesTool
+{
+    public class DBImageCache
+    {
+        private class TableInfo
+        {
+            public string TableName { get; set; }
+            public string ColumnName { get; set; }
+            public bool IsInit { get; set; }
+            public string PrimaryKey { get; set; }
+        }
+        
+        private static List<TableInfo> _list = null;
+        private static DBImageCache _obj = null;
+        private static bool _isInit = false;        
+
+        private DBImageCache()
+        {
+            _list = new List<TableInfo>();            
+        }
+
+        public static DBImageCache Instance
+        {
+            get
+            {
+                if (_obj == null)
+                {
+                    _obj = new DBImageCache();
+                }
+
+                return _obj;
+            }
+        }
+
+        public void Add(string tableName, string columnName)
+        {
+            if (_list.FirstOrDefault(item => item.TableName == tableName && item.ColumnName == columnName) != null) return;
+
+            string primaryKey = "";
+
+            switch (tableName.ToLower())
+            {
+                case "csk_store_product": primaryKey = "ProductID"; break;
+                case "csk_store_retailerproduct": primaryKey = "RetailerProductId"; break;
+                case "csk_store_image": primaryKey = "ImageID"; break;
+                case "csk_store_manufacturer": primaryKey = "ManufacturerID"; break;
+                case "csk_store_productismerged_temp": primaryKey = "MID"; break;
+                case "csk_store_category": primaryKey = "CategoryID"; break;
+                case "csk_util_country": primaryKey = "countryID"; break;
+            }
+
+            if (primaryKey == "") throw new Exception("table " + tableName + " is not configure primary key! the primary key is hard code");
+
+            _list.Add(new TableInfo() { TableName = tableName, ColumnName = columnName, IsInit = false, PrimaryKey = primaryKey });
+            _isInit = false;
+        }
+
+        public bool Compare(string fileName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(fileName)) return false;
+
+                string tempStr = Path.GetFileName(fileName);
+
+                if (string.IsNullOrEmpty(tempStr)) return false;
+                if (string.IsNullOrEmpty(Path.GetExtension(tempStr))) return false;
+
+                return IndexController.Compare(tempStr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(fileName);
+                Console.WriteLine("error:" + ex.Message + " " + ex.StackTrace);
+
+                return false;
+            }
+        }
+
+        public void Init()
+        {
+            if (_isInit) return;
+
+            this.RunTable(50000, item =>
+            {
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(item)) return;
+
+                    string fileName = Path.GetFileName(item);
+
+                    if (string.IsNullOrEmpty(fileName)) return;
+                    if (string.IsNullOrEmpty(Path.GetExtension(fileName))) return;
+
+                    IndexController.Add(fileName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(item);
+                    Console.WriteLine("error:" + ex.Message + " " + ex.StackTrace);                    
+                }
+            });
+
+            _isInit = true;
+        }
+
+        private void RunTable(int pageSize, Action<string> action)
+        {
+            _list.ForEach(item =>
+            {
+                if (item.IsInit) return;
+
+                int pageIndex = 1;
+                while (true)
+                {
+                    bool isOver = true;
+                    string sql = " select " + item.ColumnName + " from";
+                    sql += " (";
+                    sql += " select ROW_NUMBER() over(ORDER BY " + item.PrimaryKey + " asc) as xbai_num," + item.ColumnName + " from " + item.TableName;
+                    sql += " ) as a";
+                    sql += " where xbai_num BETWEEN " + ((pageIndex - 1) * pageSize + 1) + " AND " + pageIndex * pageSize;
+
+                    StoredProcedure sp = new StoredProcedure("");
+                    sp.Command.CommandSql = sql;
+                    sp.Command.CommandTimeout = 0;
+                    sp.Command.CommandType = CommandType.Text;
+                    IDataReader dr = sp.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        isOver = false;
+
+                        string val = dr[item.ColumnName].ToString();
+                        if (item.TableName.ToLower() == "csk_store_category")
+                        {
+                            val = val.Replace("_s.", ".");
+                            val = val.Replace("_ms.", ".");
+                        }
+
+                        action(val);
+                    }
+                    dr.Close();
+
+                    pageIndex++;
+                    if (isOver) break;
+                }
+            
+                item.IsInit = true;
+            });
+        }
+
+    }
+}
---
+++ Yuanxiang/CheckImagesTool/CheckImagesTool/IndexController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckImagesTool
+{
+    public class IndexController
+    {
+        private static string _indexPath = "";
+        private static Lucene.Net.Search.IndexSearcher _indexSearcher = null;
+        private static Lucene.Net.Index.IndexWriter _indexWriter = null;
+
+        static IndexController()
+        {
+            _indexPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "IndexData");
+            if (!Directory.Exists(_indexPath))
+            {
+                Directory.CreateDirectory(_indexPath);
+            }
+        }
+
+        private static Lucene.Net.Index.IndexWriter CreateIndexWriter(string indexPath)
+        {
+            Lucene.Net.Analysis.Standard.StandardAnalyzer standardAnalyzer = new Lucene.Net.Analysis.Standard.StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_29);
+            Lucene.Net.Store.FSDirectory fsDirectory = Lucene.Net.Store.FSDirectory.Open(new System.IO.DirectoryInfo(indexPath));
+            Lucene.Net.Index.IndexWriter indexWriter = new Lucene.Net.Index.IndexWriter(fsDirectory, standardAnalyzer, true, Lucene.Net.Index.IndexWriter.MaxFieldLength.UNLIMITED);
+            indexWriter.SetMergeFactor(5000);
+            indexWriter.SetMaxBufferedDocs(4000);
+            return indexWriter;
+        }
+
+        public static void Add(string data)
+        {
+            if (_indexWriter == null)
+            {
+                if (_indexSearcher != null)
+                {
+                    _indexSearcher.Close();
+                    _indexSearcher = null;
+                }
+
+                _indexWriter = CreateIndexWriter(_indexPath);
+            }
+
+            Lucene.Net.Documents.Document document = new Lucene.Net.Documents.Document();
+
+            var field = new Lucene.Net.Documents.Field("Image", data.ToLower(), Lucene.Net.Documents.Field.Store.NO, Lucene.Net.Documents.Field.Index.NOT_ANALYZED);
+            document.Add(field);
+
+            _indexWriter.AddDocument(document);
+        }
+
+        public static bool Compare(string fileName)
+        {
+            if (_indexSearcher == null)
+            {
+                if (_indexWriter != null)
+                {
+                    _indexWriter.Optimize();
+                    _indexWriter.Close();
+                    _indexWriter = null;
+                }
+
+                Lucene.Net.Store.FSDirectory fsDirectory = Lucene.Net.Store.FSDirectory.Open(new System.IO.DirectoryInfo(_indexPath));
+                _indexSearcher = new Lucene.Net.Search.IndexSearcher(fsDirectory, true);                
+            }
+
+            Lucene.Net.Search.TermQuery termQuery = new Lucene.Net.Search.TermQuery(new Lucene.Net.Index.Term("Image", fileName.ToLower()));
+
+            var doc = _indexSearcher.Search(termQuery, 100000);
+
+            return doc.TotalHits > 0;
+        }
+
+    }
+}
