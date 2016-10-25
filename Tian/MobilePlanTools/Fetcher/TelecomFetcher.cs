fa6b5e3acbfa75c73d01a3bda6e209430154218b
yuanxiang <yuanxiang@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2016-10-25 08:30:46 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@58 6c2e4e5e-b397-40cf-afe5-66bc484122c4

--- Tian/MobilePlanTools/Fetcher/TelecomFetcher.cs
+++ Tian/MobilePlanTools/Fetcher/TelecomFetcher.cs
@@ -24,122 +24,14 @@ namespace Fetcher
         {
             StartCrawlingLog();
             List<MobilePlanInfo> mobilePlanList = new List<MobilePlanInfo>();
+
             string url = "http://www.spark.co.nz/shop/mobile/mobile/plansandpricing.html";
-            StarlCrawlPlansLinkLog(url);
-            #region Phone
             string phone_url = "http://www.spark.co.nz/shop/mobile/phones.html";
-            Parser nParser = this.GetParser(phone_url);
-            var pro_obj = new HasAttributeFilter("class", "device_listing");
-            var pro_obj_list = nParser.ExtractAllNodesThatMatch(pro_obj);
+            GetData(mobilePlanList, url, phone_url, true);
 
-            var pInfos = new List<MobilePhoneInfo>();
-            if (pro_obj_list.Count > 0)
-            {
-                var pro_list = pro_obj_list[0].Children[1].Children.SearchFor(typeof(Bullet), false).ToNodeArray();
-                foreach (var pro in pro_list)
-                {
-                    var li = pro as Bullet;
-                    if (li.Children.Count < 4 || li.GetAttribute("id") == "SIMcard005" || li.GetAttribute("id") == "SIMcard006") continue;
-                    var img = li.Children.SearchFor(typeof(Bullet), true).ToNodeArray();
-                    var pro_img = (img[0].Children[0] as ImageTag).ImageURL;
-
-                    var div = img[1].Children.SearchFor(typeof(Div), false).ToNodeArray();
-                    var Name = div[0].ToPlainTextString() + " " + div[1].ToPlainTextString().Trim().Replace("\t", "").Replace("\r", "").Replace("\n", "");
-                    var str_obj = div[4].ToPlainTextString().Replace(" ", "").Replace("\t", "").Replace("\r", "").Replace("\n", "");
-                    var str_arr = str_obj.Split('$');
-                    //if (Name.Contains("Galaxy A5 Pearl White")) {
-                    //    var dsad = "";
-                    //}
-                    var Price = str_arr[1].Contains("RRP") ? int.Parse(str_arr[1].Substring(0, str_arr[1].IndexOf('R'))) : int.Parse(str_arr[1].Contains("UPFRONT") ? str_arr[1].Substring(0, str_arr[1].IndexOf('U')) : str_arr[1].Substring(0, str_arr[1].IndexOf('O')));
-
-                    var ConID = str_arr[1].Contains("24") ? 3 : 2;
-                    var Url = (li.Children[3] as ATag).Link;
-
-                    var Plan = "";
-                    if (str_arr.Length > 2) Plan = "$" + str_arr[2];
-
-                    var infos = new MobilePhoneInfo();
-                    infos.ContractTypeID = ConID;
-                    infos.PhoneName = "[" + Plan + "]" + Name;
-                    infos.UpfrontPrice = Price;
-                    infos.PhoneImage = pro_img;
-                    infos.phoneURL = Url;
-                    pInfos.Add(infos);
-                }
-
-            }
-            else
-            {
-                LogEventArgs logEventArgs = new LogEventArgs(LogType.CrawlLog, "Telecom Phone not found!", "TelecomFetcher");
-                GenerateLog(logEventArgs);
-            }
-            #endregion
-
-            JQuery doc = new JQuery(this.GetHttpContent(url), url);
-            doc.find(".clearfix.device-carousel-card").each(item => {
-                var node = item.ToJQuery();
-
-                string min = node.find(".detail-table .right").eq(1).val().Replace("mins to any NZ phone", "").Replace("to any NZ/Aus phone", "").Replace("NZ only", "");
-                string text = node.find(".detail-table .right").eq(2).val().Replace("to any NZ network", "").Replace("to any NZ/Aus network", "").Replace("texts","");
-                string dataMB = node.find(".detail-table .first-cell .right").text().Trim();
-
-                dataMB = dataMB.Replace("<span class=\"data\"> + 500MB</span>", "");
-                dataMB = dataMB.Replace("<span class=\"data\"> + 250MB</span>", "");
-                dataMB = dataMB.Replace("<span class=\"data\">", "");
-                dataMB = dataMB.Replace("</span>", "");
-
-                if (dataMB == "")
-                {
-                    dataMB = "0";
-                }
-                else
-                {
-                    if (dataMB.Contains("+"))
-                    {
-                        var strList = dataMB.Split('+');
-                        dataMB = (Convert.ToInt32(Convert.ToDecimal(strList[0].Replace("GB", "").Trim()) * 1024)).ToString();
-
-                        if (strList[1].Contains("GB^"))
-                        {
-                            dataMB = (Convert.ToInt32(dataMB) + Convert.ToInt32(Convert.ToDecimal(strList[1].Replace("GB^", "").Trim()) * 1024)).ToString();
-                        }
-                        else
-                        {
-                            dataMB = (Convert.ToInt32(dataMB) + Convert.ToDecimal(strList[1].Replace("MB", "").Trim())).ToString();
-                        }
-
-                        dataMB += "MB";
-                    }
-                }
-
-                var info = new MobilePlanInfo();
-                info.CarrierName = this.ProviderName;
-                info.DataMB = dataMB;
-                info.Minutes = min.Contains("Unlimited") ? -1 : Convert.ToInt32(min.ToDecimal());
-                info.MobilePlanName = "$" + node.find(".service-price").text().Trim().Replace("\r\n", "").Replace("\t", "").Replace("/MTH", "");
-                info.MobilePlanURL = url;
-                info.Price = node.find(".service-price .number").text().ToDecimal();
-                info.Texts = text.Contains("Unlimited") ? -1 : Convert.ToInt32(text.ToDecimal());
-                info.plus = 0;
-                var PhoneList = new List<MobilePhoneInfo>();
-                foreach (var p in pInfos)
-                {
-                    if (p.PhoneName.Contains("]") || p.PhoneName.Contains("["))
-                    {
-                        var plan_name = p.PhoneName.Substring(p.PhoneName.IndexOf('[') + 1, p.PhoneName.LastIndexOf(']') - 1);
-                        if (info.MobilePlanName == plan_name)
-                        {
-                            p.PhoneName = p.PhoneName.Replace("[" + plan_name + "]", "");
-                            PhoneList.Add(pInfos[pInfos.IndexOf(p)]);
-                        }
-                    }
-
-                }
-                info.Phones = PhoneList;                
-
-                mobilePlanList.Add(info);
-            });
-
+            //url = "http://www.spark.co.nz/shop/mobile/mobilebroadband/plansandpricing.html";
+            //phone_url = "http://www.spark.co.nz/shop/mobile/mobilebroadbandandtablets/devices.html";
+            //GetData(mobilePlanList, url, phone_url, false);
 
 
             #region PhonePlan
@@ -257,7 +149,137 @@ namespace Fetcher
             return mobilePlanList;
         }
 
+        private void GetData(List<MobilePlanInfo> mobilePlanList, string url, string phone_url, bool isPlan)
+        {
+            StarlCrawlPlansLinkLog(url);
+            #region Phone
+            Parser nParser = this.GetParser(phone_url);
+            var pro_obj = new HasAttributeFilter("class", "device_listing");
+            var pro_obj_list = nParser.ExtractAllNodesThatMatch(pro_obj);
 
+            var pInfos = new List<MobilePhoneInfo>();
+            if (pro_obj_list.Count > 0)
+            {
+                var pro_list = pro_obj_list[0].Children[1].Children.SearchFor(typeof(Bullet), false).ToNodeArray();
+                foreach (var pro in pro_list)
+                {
+                    try
+                    {
+                        var li = pro as Bullet;
+                        if (li.Children.Count < 4 || li.GetAttribute("id") == "SIMcard005" || li.GetAttribute("id") == "SIMcard006") continue;
+                        var img = li.Children.SearchFor(typeof(Bullet), true).ToNodeArray();
+                        var pro_img = (img[0].Children[0] as ImageTag).ImageURL;
 
+                        var div = img[1].Children.SearchFor(typeof(Div), false).ToNodeArray();
+                        var Name = div[0].ToPlainTextString() + " " + div[1].ToPlainTextString().Trim().Replace("\t", "").Replace("\r", "").Replace("\n", "");
+                        var str_obj = div[4].ToPlainTextString().Replace(" ", "").Replace("\t", "").Replace("\r", "").Replace("\n", "");
+                        var str_arr = str_obj.Split('$');
+                        //if (Name.Contains("Galaxy A5 Pearl White")) {
+                        //    var dsad = "";
+                        //}
+                        decimal price = 0m;
+                        decimal.TryParse(str_arr[1].Replace("RRP", "").Replace("UPFRONT", "").Replace("Plus", ""), out price);
+                        if (price == 0m)
+                            price = str_arr[1].Contains("RRP") ? int.Parse(str_arr[1].Substring(0, str_arr[1].IndexOf('R'))) : int.Parse(str_arr[1].Contains("UPFRONT") ? str_arr[1].Substring(0, str_arr[1].IndexOf('U')) : str_arr[1].Substring(0, str_arr[1].IndexOf('O')));
+
+                        var ConID = str_arr[1].Contains("24") ? 3 : 2;
+                        var Url = (li.Children[3] as ATag).Link;
+
+                        var Plan = "";
+                        if (str_arr.Length > 2) Plan = "$" + str_arr[2];
+
+                        var infos = new MobilePhoneInfo();
+                        infos.ContractTypeID = ConID;
+                        infos.PhoneName = "[" + Plan + "]" + Name;
+                        infos.UpfrontPrice = price;
+                        infos.PhoneImage = pro_img;
+                        infos.phoneURL = Url;
+                        pInfos.Add(infos);
+                    }
+                    catch { }
+                }
+
+            }
+            else
+            {
+                LogEventArgs logEventArgs = new LogEventArgs(LogType.CrawlLog, "Telecom Phone not found!", "TelecomFetcher");
+                GenerateLog(logEventArgs);
+            }
+            #endregion
+
+            GetMobilePlan(mobilePlanList, pInfos, url);
+
+            url = "http://www.spark.co.nz/shop/mobile/prepaid.html";
+            GetMobilePlan(mobilePlanList, pInfos, url);
+        }
+
+        private void GetMobilePlan(List<MobilePlanInfo> mobilePlanList, List<MobilePhoneInfo> pInfos, string url)
+        {
+            JQuery doc = new JQuery(this.GetHttpContent(url), url);
+            doc.find(".clearfix.device-carousel-card").each(item =>
+            {
+                var node = item.ToJQuery();
+
+                string min = node.find(".detail-table .right").eq(1).val().Replace("mins to any NZ phone", "").Replace("to any NZ/Aus phone", "").Replace("NZ only", "");
+                string text = node.find(".detail-table .right").eq(2).val().Replace("to any NZ network", "").Replace("to any NZ/Aus network", "").Replace("texts", "");
+                string dataMB = node.find(".detail-table .first-cell .right").text().Trim();
+
+                dataMB = dataMB.Replace("<span class=\"data\"> + 500MB</span>", "");
+                dataMB = dataMB.Replace("<span class=\"data\"> + 250MB</span>", "");
+                dataMB = dataMB.Replace("<span class=\"data\">", "");
+                dataMB = dataMB.Replace("</span>", "");
+
+                if (dataMB == "")
+                {
+                    dataMB = "0";
+                }
+                else
+                {
+                    if (dataMB.Contains("+"))
+                    {
+                        var strList = dataMB.Split('+');
+                        dataMB = (Convert.ToInt32(Convert.ToDecimal(strList[0].Replace("GB", "").Trim()) * 1024)).ToString();
+
+                        if (strList[1].Contains("GB^"))
+                        {
+                            dataMB = (Convert.ToInt32(dataMB) + Convert.ToInt32(Convert.ToDecimal(strList[1].Replace("GB^", "").Trim()) * 1024)).ToString();
+                        }
+                        else
+                        {
+                            dataMB = (Convert.ToInt32(dataMB) + Convert.ToDecimal(strList[1].Replace("MB", "").Trim())).ToString();
+                        }
+
+                        dataMB += "MB";
+                    }
+                }
+
+                var info = new MobilePlanInfo();
+                info.CarrierName = this.ProviderName;
+                info.DataMB = dataMB;
+                info.Minutes = min.Contains("Unlimited") ? -1 : Convert.ToInt32(min.ToDecimal());
+                info.MobilePlanName = "$" + node.find(".service-price").text().Trim().Replace("\r\n", "").Replace("\t", "").Replace("/MTH", "");
+                info.MobilePlanURL = url;
+                info.Price = node.find(".service-price .number").text().ToDecimal();
+                info.Texts = text.Contains("Unlimited") ? -1 : Convert.ToInt32(text.ToDecimal());
+                info.plus = 0;
+                var PhoneList = new List<MobilePhoneInfo>();
+                foreach (var p in pInfos)
+                {
+                    if (p.PhoneName.Contains("]") || p.PhoneName.Contains("["))
+                    {
+                        var plan_name = p.PhoneName.Substring(p.PhoneName.IndexOf('[') + 1, p.PhoneName.LastIndexOf(']') - 1);
+                        plan_name = plan_name.Replace("OpenTerm", "").Replace("Plus", "").Replace("Value Pack", "").Trim();
+                        if (info.MobilePlanName == plan_name)
+                        {
+                            p.PhoneName = p.PhoneName.Replace("[" + plan_name + "]", "");
+                            PhoneList.Add(pInfos[pInfos.IndexOf(p)]);
+                        }
+                    }
+                }
+                info.Phones = PhoneList;
+
+                mobilePlanList.Add(info);
+            });
+        }
     }
 }
