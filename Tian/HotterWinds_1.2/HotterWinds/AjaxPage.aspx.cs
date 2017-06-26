df0a648d8940efea14a70dc336ceb92518c2e2cf
tianbj <tianbj@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2017-06-26 23:46:10 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@129 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Tian/HotterWinds_1.2/HotterWinds/AjaxPage.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using PriceMeChannel.App_Code_Not.Ajax;
+
+namespace HotterWinds
+{
+    public partial class AjaxPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string className = PriceMe.Utility.GetParameter("controller");
+                string methodName = PriceMe.Utility.GetParameter("action");
+                string param = PriceMe.Utility.GetParameter("param");
+                string fromUrl = PriceMe.Utility.GetParameter("url");
+
+                string userId = "";
+                //var user = this.CurrentUser;
+                //if (user != null && user.ProviderUserKey != null)
+                //{
+                //    userId = user.ProviderUserKey.ToString();
+                //}
+
+                if (string.IsNullOrEmpty(className) || string.IsNullOrEmpty(methodName))
+                {
+                    Response.Write("0");
+                }
+                else
+                {
+                    var tempParam = JsonConvert.DeserializeObject<Dictionary<string, object>>(param);
+
+                    object obj = AjaxBuilder.Ajax(HttpContext.Current, className, methodName, tempParam, fromUrl, userId);
+
+                    Response.Write(obj);
+                }
+            }
+            catch
+            {
+                Response.Write("0");
+            }
+            finally
+            {
+                Response.End();
+            }
+        }
+    }
+}
