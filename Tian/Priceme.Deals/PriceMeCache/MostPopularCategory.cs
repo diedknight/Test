40390be5dd6174400ad88e8630e1ed1361c99723
zhenglei <zhenglei@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2017-07-24 08:17:50 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@179 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Tian/Priceme.Deals/PriceMeCache/MostPopularCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PriceMeCache
+{
+    public class MostPopularCategory
+    {
+        public int RootID { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public int Cnt { get; set; }
+
+        public int Rn { get; set; }
+    }
+}
