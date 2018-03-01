266973e543981a91f4d7e4ed56eeecafbd6b06e9
zhenglei <zhenglei@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2018-03-01 08:45:53 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@334 6c2e4e5e-b397-40cf-afe5-66bc484122c4

--- Tian/MobilePlanTools/Fetcher/TelecomFetcher.cs
+++ Tian/MobilePlanTools/Fetcher/TelecomFetcher.cs
@@ -75,10 +75,10 @@ namespace Fetcher
             GetMobilePlan(mobilePlanList, url);
 
             url = "https://www.spark.co.nz/shop/mobile-plans/prepaid.html";
-            GetMobilePlan(mobilePlanList, url);
+            GetPrepaidMobilePlan(mobilePlanList, url);
         }
 
-        private void GetMobilePlan(List<MobilePlanInfo> mobilePlanList, string url)
+        private void GetPrepaidMobilePlan(List<MobilePlanInfo> mobilePlanList, string url)
         {
             string phonesUrlBase = "https://www.spark.co.nz/shop/mobile/phones.html?contr=phones&tab=-1&source=SOPO_ServicePlan&servicePlanId=";
 
@@ -90,12 +90,12 @@ namespace Fetcher
                 string min = node.find(".detail-table .right").eq(1).val().Replace("mins to any NZ phone", "").Replace("to any NZ/Aus phone", "").Replace("NZ only", "").Replace("NZ Rollover Mins*", "");
                 string text = node.find(".detail-table .right").eq(2).val().Replace("to any NZ network", "").Replace("to any NZ/Aus network", "").Replace("texts", "");
                 string dataMB = node.find(".detail-table .first-cell .right").text().Trim();
-
+                string nameMB = "";
                 dataMB = dataMB.Replace("<span class=\"data\"> + 500MB</span>", "");
                 dataMB = dataMB.Replace("<span class=\"data\"> + 250MB</span>", "");
                 dataMB = dataMB.Replace("<span class=\"data\">", "");
                 dataMB = dataMB.Replace("</span>", "");
-                dataMB = dataMB.Replace("Rollover Data", "").Replace("Bonus Data*", "").Replace("Rollover", "").Replace("Socialiser*", "").Replace("*", "");
+                dataMB = dataMB.Replace("Rollover Data", "").Replace("Bonus Data*", "").Replace("Rollover", "").Replace("Socialiser*", "").Replace("*", "").Trim();
 
                 if (dataMB == "")
                 {
@@ -110,6 +110,12 @@ namespace Fetcher
                         dataMB = (Convert.ToDecimal(strList[0].Replace("GB", "").Trim()) + Convert.ToDecimal(strList[1].Replace("GB", "").Trim())).ToString("0.0");
 
                         dataMB += "GB";
+
+                        nameMB = strList[0].Trim();
+                    }
+                    else
+                    {
+                        nameMB = dataMB;
                     }
                 }
 
@@ -118,14 +124,16 @@ namespace Fetcher
                 info.DataMB = dataMB;
                 info.Minutes = min.Contains("Unlimited") ? -1 : Convert.ToInt32(min.ToDecimal());
                 info.MobilePlanName = "$" + node.find(".service-price").text().Trim().Replace("\r\n", "").Replace("\t", "").Replace("/MTH", "");
-                string otherNames = node.find(".pink-tag").text().Trim();
-                info.MobilePlanName = otherNames + " " + info.MobilePlanName;
+
                 info.MobilePlanURL = url;
                 info.Price = node.find(".service-price .number").text().ToDecimal();
                 info.Texts = text.Contains("Unlimited") ? -1 : Convert.ToInt32(text.ToDecimal());
                 info.plus = 0;
+
+                info.MobilePlanName = "Prepaid " + dataMB;
+
                 var PhoneList = new List<MobilePhoneInfo>();
-                string priceInfo = info.Price.ToString("0.00");
+
 
                 string phonesUrlFlag = node.find(".hightlight-text.view-mobi-add").attr("data-serviceplanid");
                 string phonesUrl = phonesUrlBase + phonesUrlFlag;
@@ -137,18 +145,136 @@ namespace Fetcher
                     var phoneNode = phonesItem.ToJQuery();
                     if (phoneNode.get(0).OuterHtml.StartsWith("<spark-device-card"))
                     {
-                        var infos = new MobilePhoneInfo();
+                        var mpInfo = new MobilePhoneInfo();
                         //infos.ContractTypeID = node.attr("prop-id");
-                        infos.PhoneName = phoneNode.attr("prop-brandName") + " " + phoneNode.attr("prop-name");
-                        infos.UpfrontPrice = phoneNode.attr("prop-deferredPrice").ToDecimal();
-                        infos.PhoneImage = phoneNode.attr("prop-productImage");
-                        Uri imageAbsoluteUri = new Uri(new Uri("https://www.spark.co.nz/"), infos.PhoneImage);
-                        infos.PhoneImage = imageAbsoluteUri.ToString();
-                        infos.phoneURL = phoneNode.attr("prop-cta");
-                        Uri productAbsoluteUri = new Uri(new Uri("https://www.spark.co.nz/"), infos.phoneURL);
-                        infos.phoneURL = productAbsoluteUri.ToString();
-                        infos.PlanName = phoneNode.attr("prop-listedPlanName");
-                        pInfos.Add(infos);
+                        mpInfo.PhoneName = phoneNode.attr("prop-brandName") + " " + phoneNode.attr("prop-name");
+                        mpInfo.UpfrontPrice = phoneNode.attr("prop-deferredPrice").ToDecimal();
+                        mpInfo.PhoneImage = phoneNode.attr("prop-productImage");
+                        Uri imageAbsoluteUri = new Uri(new Uri("https://www.spark.co.nz/"), mpInfo.PhoneImage);
+                        mpInfo.PhoneImage = imageAbsoluteUri.ToString();
+                        mpInfo.phoneURL = phoneNode.attr("prop-cta");
+                        Uri productAbsoluteUri = new Uri(new Uri("https://www.spark.co.nz/"), mpInfo.phoneURL);
+                        mpInfo.phoneURL = productAbsoluteUri.ToString();
+                        mpInfo.PlanName = phoneNode.attr("prop-listedPlanName");
+                        mpInfo.ContractTypeID = 1;
+                        pInfos.Add(mpInfo);
+                    }
+                });
+                info.Phones = pInfos;
+
+                mobilePlanList.Add(info);
+            });
+        }
+
+        private void GetMobilePlan(List<MobilePlanInfo> mobilePlanList, string url)
+        {
+            string phonesUrlBase = "https://www.spark.co.nz/shop/mobile/phones.html?contr=phones&tab=-1&source=SOPO_ServicePlan&servicePlanId=";
+
+            JQuery doc = new JQuery(this.GetHttpContent(url), url);
+            doc.find(".clearfix.device-carousel-card").each(item =>
+            {
+                var node = item.ToJQuery();
+
+                string min = node.find(".detail-table .right").eq(1).val().Replace("mins to any NZ phone", "").Replace("to any NZ/Aus phone", "").Replace("NZ only", "").Replace("NZ Rollover Mins*", "");
+                string text = node.find(".detail-table .right").eq(2).val().Replace("to any NZ network", "").Replace("to any NZ/Aus network", "").Replace("texts", "");
+                string dataMB = node.find(".detail-table .first-cell .right").text().Trim();
+                string nameMB = "";
+                dataMB = dataMB.Replace("<span class=\"data\"> + 500MB</span>", "");
+                dataMB = dataMB.Replace("<span class=\"data\"> + 250MB</span>", "");
+                dataMB = dataMB.Replace("<span class=\"data\">", "");
+                dataMB = dataMB.Replace("</span>", "");
+                dataMB = dataMB.Replace("Rollover Data", "").Replace("Bonus Data*", "").Replace("Rollover", "").Replace("Socialiser*", "").Replace("*", "").Trim();
+
+                if (dataMB == "")
+                {
+                    dataMB = "0";
+                }
+                else
+                {
+                    if (dataMB.Contains("+"))
+                    {
+                        var strList = dataMB.Split('+');
+
+                        dataMB = (Convert.ToDecimal(strList[0].Replace("GB", "").Trim()) + Convert.ToDecimal(strList[1].Replace("GB", "").Trim())).ToString("0.0");
+
+                        dataMB += "GB";
+
+                        nameMB = strList[0].Trim();
+                    }
+                    else
+                    {
+                        nameMB = dataMB;
+                    }
+                }
+
+                if(nameMB.Contains("Unlimited"))
+                {
+                    nameMB = "Unlimited Data";
+                }
+
+                var info = new MobilePlanInfo();
+                info.CarrierName = this.ProviderName;
+                info.DataMB = dataMB;
+                info.Minutes = min.Contains("Unlimited") ? -1 : Convert.ToInt32(min.ToDecimal());
+                info.MobilePlanName = "$" + node.find(".service-price").text().Trim().Replace("\r\n", "").Replace("\t", "").Replace("/MTH", "");
+                
+                info.MobilePlanURL = url;
+                info.Price = node.find(".service-price .number").text().ToDecimal();
+                info.Texts = text.Contains("Unlimited") ? -1 : Convert.ToInt32(text.ToDecimal());
+                info.plus = 0;
+
+                string otherNames = node.find(".pink-tag").text().Trim();
+                if (otherNames.Contains("12 Mths"))
+                {
+                    info.MobilePlanName = "12 Months " + nameMB + " $" + info.Price.ToString("0.00");
+                    info.ContractTypeId = 2;
+                }
+                else if (otherNames.Contains("24 Mths"))
+                {
+                    info.MobilePlanName = "24 Months " + nameMB + " $" + info.Price.ToString("0.00");
+                    info.ContractTypeId = 3;
+                }
+                else if(otherNames.Contains("Unlimited"))
+                {
+                    info.MobilePlanName = nameMB + " $" + info.Price.ToString("0.00");
+                    info.ContractTypeId = 1;
+                }
+                else if (otherNames.Contains("New Rollover Plans"))
+                {
+                    info.MobilePlanName = "24 Months " + nameMB + " $" + info.Price.ToString("0.00");
+                    info.ContractTypeId = 3;
+                }
+                else
+                {
+                    info.MobilePlanName = otherNames + " " + info.MobilePlanName;
+
+                }
+
+                var PhoneList = new List<MobilePhoneInfo>();
+
+                string phonesUrlFlag = node.find(".hightlight-text.view-mobi-add").attr("data-serviceplanid");
+                string phonesUrl = phonesUrlBase + phonesUrlFlag;
+                JQuery phonesDoc = new JQuery(this.GetHttpContent(phonesUrl), phonesUrl);
+
+                List<MobilePhoneInfo> pInfos = new List<MobilePhoneInfo>();
+                phonesDoc.find("#entry > .container-fluid > *").each(phonesItem =>
+                {
+                    var phoneNode = phonesItem.ToJQuery();
+                    if (phoneNode.get(0).OuterHtml.StartsWith("<spark-device-card"))
+                    {
+                        var mpInfo = new MobilePhoneInfo();
+                        //infos.ContractTypeID = node.attr("prop-id");
+                        mpInfo.PhoneName = phoneNode.attr("prop-brandName") + " " + phoneNode.attr("prop-name");
+                        mpInfo.UpfrontPrice = phoneNode.attr("prop-deferredPrice").ToDecimal();
+                        mpInfo.PhoneImage = phoneNode.attr("prop-productImage");
+                        Uri imageAbsoluteUri = new Uri(new Uri("https://www.spark.co.nz/"), mpInfo.PhoneImage);
+                        mpInfo.PhoneImage = imageAbsoluteUri.ToString();
+                        mpInfo.phoneURL = phoneNode.attr("prop-cta");
+                        Uri productAbsoluteUri = new Uri(new Uri("https://www.spark.co.nz/"), mpInfo.phoneURL);
+                        mpInfo.phoneURL = productAbsoluteUri.ToString();
+                        mpInfo.PlanName = phoneNode.attr("prop-listedPlanName");
+                        mpInfo.ContractTypeID = info.ContractTypeId;
+                        pInfos.Add(mpInfo);
                     }
                 });
                 info.Phones = pInfos;
