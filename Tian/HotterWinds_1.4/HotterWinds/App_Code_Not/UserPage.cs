e4982d2820c3253fe166f5be4f5354159f694fea
tianbj <tianbj@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2018-05-25 01:15:25 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@374 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Tian/HotterWinds_1.4/HotterWinds/App_Code_Not/UserPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotterWinds.App_Code_Not
+{
+    public class UserPage : System.Web.UI.Page
+    {
+        public UserData UserData = null;
+
+
+        protected override void OnPreLoad(EventArgs e)
+        {
+            this.UserData = PriceMe.Utility.GetUserInfoFromCookie();
+
+            base.OnPreLoad(e);
+        }
+    }
+}
