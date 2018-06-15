0bce0bffe7991d8317b38e937f41aba88b66395f
tianbj <tianbj@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2018-06-15 05:46:06 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@394 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Tian/GEPREmail/GEPREmail/Data/WeeklyDealsUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GEPREmail.Data
+{
+    public class WeeklyDealsUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public bool IsAcceptGDPR { get; set; }
+    }
+}
---
+++ Tian/GEPREmail/GEPREmail/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using GEPREmail.Data;
+using GEPREmail.EmailTemplate;
+using Priceme.Infrastructure.Email;
+
+namespace GEPREmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<WeeklyDealsUser> list = new List<WeeklyDealsUser>();
+
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["PriceMeTemplate"].ConnectionString))
+            {
+                string sql = "select * from WeeklyDealsUser";
+
+                list = con.Query<WeeklyDealsUser>(sql).ToList();
+            }
+
+            list.ForEach(item => {
+                if (string.IsNullOrEmpty(item.EmailAddress))
+                {
+                    return;
+                }
+
+                item.Name = item.Name ?? "";
+
+                string html = Template.Load("GDPR.html");
+                html = html.Replace("{firstname}", item.Name);
+                html = html.Replace("{id}", item.Id.ToString());
+
+                //发送邮件
+                string emailStr = ConfigurationManager.AppSettings["Email"];
+                string wSAccessKey = ConfigurationManager.AppSettings["WSAccessKey"];
+                string wSSecretKey = ConfigurationManager.AppSettings["WSSecretKey"];
+                string title= ConfigurationManager.AppSettings["Title"];
+
+                IEmail email = new AmazonEmail(emailStr, wSAccessKey, wSSecretKey);
+                email.Send(title, html, item.EmailAddress);
+            });
+
+
+        }
+    }
+}
