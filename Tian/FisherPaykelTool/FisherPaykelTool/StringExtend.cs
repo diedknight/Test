2b260a60ce8d8163ee21e581a9668f50d3d71262
tianbj <tianbj@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2018-08-06 09:40:04 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@429 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Tian/FisherPaykelTool/FisherPaykelTool/StringExtend.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FisherPaykelTool
+{
+    public static class StringExtend
+    {
+        public static decimal ToDecimal(this string str, decimal defVal = 0m)
+        {
+            decimal val = defVal;
+            string valStr = str;
+
+            if (string.IsNullOrEmpty(valStr)) return val;
+
+            valStr = valStr.Replace("SGD", "");
+            valStr = valStr.Replace("Php", "");
+            valStr = valStr.Replace("USD", "");
+            valStr = valStr.Replace("MYR", "");
+            valStr = valStr.Replace("RRP", "");
+            valStr = valStr.Replace("Rp.", "");
+            valStr = valStr.Replace("Rp", "");
+            valStr = valStr.Replace("SG", "");
+            valStr = valStr.Replace("RM", "");
+            valStr = valStr.Replace("$", "");
+            valStr = valStr.Replace("đ", "");
+            valStr = valStr.Replace("₫", "");
+            valStr = valStr.Replace("₱", "");
+            valStr = valStr.Replace("฿", "");
+            valStr = valStr.Replace(",", "");
+            valStr = valStr.Replace(":", "");
+
+            valStr = valStr.Replace(Environment.NewLine, "");
+            valStr = valStr.Replace("\n", "");
+            valStr = valStr.Replace(" ", "");
+            decimal.TryParse(valStr.Trim(), out val);
+
+            return val;
+        }
+
+        public static string TrimA(this string str)
+        {
+            str = str.Replace(Environment.NewLine, " ").Replace("\n", " ").Replace("\r", " ").Trim();
+            
+            StringBuilder sb = new StringBuilder();
+            bool isRepeatWhiteSpace = false;
+
+            foreach (char c in str)
+            {
+                if (!char.IsWhiteSpace(c))
+                {
+                    isRepeatWhiteSpace = false;
+                    sb.Append(c);                    
+                }
+                else if (!isRepeatWhiteSpace)
+                {
+                    isRepeatWhiteSpace = true;
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string TirmStr(this string str)
+        {
+
+            if (string.IsNullOrEmpty(str)) return "";
+
+            string getStr = str.Replace("\n", "").Replace("/n", "").TrimStart(' ');
+
+            if (str.StartsWith(" "))
+                TirmStr(getStr);
+
+            getStr = getStr.TrimEnd(' ');
+
+            if (str.EndsWith(" "))
+                TirmStr(getStr);
+
+            return getStr;
+        }
+
+        //public static string GetLargeImg(this string img,string[] imgs)
+        //{
+
+        //    if (string.IsNullOrEmpty(img)||imgs.Length<=0) return "";
+
+        //    var pro_img = img;
+        //    imgs.ToList().ForEach(f => {
+        //        var str = f.Split(',');
+        //        if(str.Length>=2)
+        //            pro_img = pro_img.Replace(str[0], str[1]);
+        //    });
+
+        //    return pro_img;
+        //}
+    }
+}
