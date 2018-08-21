a00c557397cda479c6f32a42d887c2683b318892
tianbj <tianbj@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2018-08-21 08:26:40 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@442 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Tian/HandleBecexTech/HandleBecexTech/AppConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandleBecexTech
+{
+    public class AppConfig
+    {
+        public static string FeedFileURL { get; set; }
+        public static string NewFeedPath { get; set; }
+        public static string NewFeedName { get; set; }
+        public static string LogPath { get; set; }
+
+        static AppConfig()
+        {
+            FeedFileURL = ConfigurationManager.AppSettings["FeedFileURL"];
+            NewFeedPath = ConfigurationManager.AppSettings["NewFeedPath"];
+            NewFeedName = ConfigurationManager.AppSettings["NewFeedName"];
+            LogPath = ConfigurationManager.AppSettings["LogPath"];
+        }
+
+
+    }
+}
---
+++ Tian/HandleBecexTech/HandleBecexTech/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace HandleBecexTech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var bts = wc.DownloadData(AppConfig.FeedFileURL);
+
+                using (MemoryStream ms = new MemoryStream(bts))
+                {
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(ms);
+                    var rootNode = doc.SelectSingleNode("Feed");
+                    var nodes = doc.SelectNodes("Feed/Product");
+
+                    foreach (XmlNode node in nodes)
+                    {
+                        try
+                        {
+                            var priceNode = node.SelectSingleNode("Price");
+                            var price = Convert.ToDecimal(priceNode.InnerText);
+
+                            if (price >= 400)
+                            {
+                                rootNode.RemoveChild(node);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            string logFile = Path.Combine(AppConfig.NewFeedPath, "log" + DateTime.Now.ToString("yyyy_MM_dd") + ".txt");
+
+                            XbaiLog.WriteLog(logFile, ex.Message);
+                            XbaiLog.WriteLog(logFile, ex.StackTrace);
+                        }
+                    }
+
+                    string fileName = Path.Combine(AppConfig.NewFeedPath, AppConfig.NewFeedName);
+                    doc.Save(fileName);
+                }
+
+            }
+
+        }
+    }
+}
