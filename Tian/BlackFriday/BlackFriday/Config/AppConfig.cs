ea262cdf687702b0d5751c7ff414dd018eb6f90e
tianbj <tianbj@6c2e4e5e-b397-40cf-afe5-66bc484122c4>
2018-10-18 08:48:59 +0000

    git-svn-id: https://priceme.unfuddle.com/svn/priceme_minitools@476 6c2e4e5e-b397-40cf-afe5-66bc484122c4

---
+++ Tian/BlackFriday/BlackFriday/Config/AppConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackFriday.Config
+{
+    public class AppConfig
+    {
+        public DbInfo PricemeDBConnection { get; private set; }
+
+        public AppConfig(IConfiguration configuration)
+        {
+            this.PricemeDBConnection = configuration.GetSection("PricemeDbInfo").Get<DbInfo>();
+        }
+    }
+}
---
+++ Tian/BlackFriday/BlackFriday/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlackFriday.Config;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace BlackFriday.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private AppConfig _appConfig = null;
+
+        public string Description { get; set; }
+
+        public IndexModel(AppConfig appConfig)
+        {
+            this._appConfig = appConfig;
+        }
+
+
+        public void OnGet()
+        {
+            using (var con = new MySqlConnection(this._appConfig.PricemeDBConnection.ConnectionString))
+            {
+                string sql = "select Ctx from CSK_Content where PageId='Black Friday information'";
+
+                this.Description = con.ExecuteScalar<string>(sql);
+            }
+        }
+
+        public void OnPost(string txtEmail)
+        {            
+            using (var con = new MySqlConnection(this._appConfig.PricemeDBConnection.ConnectionString))
+            {
+                string sql = "INSERT INTO WeeklyDealsUser (emailaddress) VALUES (@email)";
+
+                con.Execute(sql, new { email = txtEmail });
+            }
+
+            this.ViewData["signup"] = "Thank you for signing up";
+            OnGet();
+        }
+
+    }
+}
